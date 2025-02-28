f05de974e93117377b7beb3237b00ca476b30e69
NIKANORE09 <nikolo44335@gmail.com>
2025-02-28 23:35:19 +0400

    add styles and done

--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -1,26 +1,23 @@
-import React from "react";
 import React, { useState } from "react";
 
-export default function Home({data, setDisplayState}) {
+export default function Home({ data, setDisplayState }) {
   console.log("data", data);
 
   const handleClick = () => {
-    setDisplayState('questions')
-  }
-
+    setDisplayState("questions");
+  };
 
   return (
-    <div className="flex gap-5 w-full">
+    <div className="flex gap-5 w-60">
       <div>
         <h1 className=" text-bold text-2xl">quiz app</h1>
       </div>
       <div className=" widht-full">
         {data.map((el) => (
-          <div 
-          onClick={handleClick}
-
-
-          className=" cursor-pointer h-10 w-1/2 border-2 hover:bg-blue-850">
+          <div
+            onClick={handleClick}
+            className=" cursor-pointer h-10 w-60 border-2 hover:bg-blue-800"
+          >
             {el.title}
           </div>
         ))}
