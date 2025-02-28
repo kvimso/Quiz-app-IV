da21fcf34926293d7b3e9e1a554f91f4ffa89333
elisabedi <elisabedbakanidze51@gmail.com>
2025-02-28 22:56:07 +0400

    question

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./comps/Home";
 import { data } from "./Question.js";
 
 function App() {
-  //ელისაბეთის ნაწილი დაწყების დრო 28:20
+  const [displayState, setDisplayState] = useState('home')
+  const [topic, setTopic] = useState('')
+//ელისაბეთის ნაწილი დაწყების დრო 28:20
+
   return (
     <>
       <h1 className="w2">header</h1>
-      <Home />
+      {displayState === 'home' && <Home setDisplayState={setDisplayState} data={data} />}
       <h1>questions</h1>
     </>
   );
