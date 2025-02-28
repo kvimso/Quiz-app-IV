cf66cf3063bbd3b5a9b0b37fa583c065150e4217
NIKANORE09 <nikolo44335@gmail.com>
2025-02-28 20:40:25 +0400

    main page(home page)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Home from "./comps/Home";
+import { data } from "./Question.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //ელისაბეთის ნაწილი დაწყების დრო 28:20
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="w2">header</h1>
+      <Home />
+      <h1>questions</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// მეცხრე ხაზი შეცვალე ამით : <Home data={data} />
