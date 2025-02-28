55d9279bb2f09dc8d45d0c092c5cd8fa4862560d
kvimso <kvimsina@gmail.com>
2025-02-28 22:05:25 +0400

    questions

--- src/Question.js
+++ src/Question.js
@@ -1,1 +1,21 @@
 //გიორგის ნაწილი იწყება დრო 9:55 სრულდება დრო 15:15
+
+const questions = {
+    "title": "HTML",
+    //"icon": "./assets/icon-html.svg",
+    "bg": "#FFF1E9",
+    "questions": [
+        {
+            "question": "What does HTML stand for?",
+            "options": [
+                "Hyper Trainer Marketing Language",
+                "Hyper Text Marketing Language",
+                "Hyper Text Markup Language",
+                "Hyper Text Markup Leveler",
+            ],
+            "answer": "Hyper Text Markup Language",
+        }
+    ]
+
+
+}
